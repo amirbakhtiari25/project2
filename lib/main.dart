a281161a685f82f2e189c12714ab6bd1a2a0053d
amirbakhtiari25 <ammahrobakhtiyari@gmail.com>
2025-03-31 14:53:53 +0330

    first project

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
 
+PS D:\flatt\progect\project2> git config --global user.name "amirbakhtiari25"
+PS D:\flatt\progect\project2> git config --global user.email "ammahrobakhtiyari@gmail.com"
 
